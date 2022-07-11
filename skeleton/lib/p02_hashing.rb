0436ee90181aad9989f63b54b508e3a56fdd06e0
TaoweiLi <104708757+TaoweiLi@users.noreply.github.com>
2022-07-11 15:37:07 -0700

    finished hashing

--- skeleton/lib/p02_hashing.rb
+++ skeleton/lib/p02_hashing.rb
@@ -4,11 +4,16 @@ end
 
 class Array
   def hash
+    temp_str = "#{self}"
+    temp_str.hash
   end
 end
 
 class String
   def hash
+    temp_arr = self.split("")
+    hashint = temp_arr.inject(0) { |acc, ele| acc * Math::PI.to_s[acc % 17].to_i + ele.ord }
+    return hashint.hash
   end
 end
 
@@ -16,6 +21,7 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    temp_str = "#{self.sort}"
+    temp_str.hash
   end
 end
