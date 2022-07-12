4598c9b5b208fb081387e85897e7db53bd145837
canjalal <vincent.shuali@gmail.com>
2022-07-11 17:26:06 -0700

    Finish linked list

--- skeleton/lib/p03_hash_set.rb
+++ skeleton/lib/p03_hash_set.rb
@@ -7,18 +7,30 @@ class HashSet
   end
 
   def insert(key)
+    self.resize! if count == num_buckets
+    unless self.include?(key)
+      @count += 1
+      @store[key.hash % num_buckets] << key unless self.include?(key)
+    end
   end
 
   def include?(key)
+    self[key].include?(key)
+    
   end
 
   def remove(key)
+    if self[key].include?(key)
+      self[key].delete(key)
+      @count -= 1
+    end
   end
 
   private
 
-  def [](num)
-    # optional but useful; return the bucket corresponding to `num`
+  def [](key)
+    # optional but useful; return the bucket corresponding to `key`
+    @store[key.hash % num_buckets]
   end
 
   def num_buckets
@@ -26,5 +38,11 @@ class HashSet
   end
 
   def resize!
+    num_buckets = @store.length*2
+    temp = Array.new(num_buckets) {Array.new}
+    @store.flatten.each do |key| # 1-level flatten should be O(n)
+      temp[key.hash % num_buckets] << key
+    end
+    @store = temp # O(1) because we're just reassigning the pointer
   end
 end
--- skeleton/lib/p04_linked_list.rb
+++ skeleton/lib/p04_linked_list.rb
@@ -14,13 +14,24 @@ class Node
   end
 
   def remove
+    temp = @prev
+    temp.next = @next
+    temp = @next
+    temp.prev = @prev
+    @prev = nil
+    @next = nil
     # optional but useful, connects previous link to next link
     # and removes self from list.
   end
 end
 
 class LinkedList
+  include Enumerable
   def initialize
+    @head = Node.new
+    @tail = Node.new
+    @head.next = @tail
+    @tail.prev = @head
   end
 
   def [](i)
@@ -29,30 +40,70 @@ class LinkedList
   end
 
   def first
+    self.empty? ? nil : @head.next
+
   end
 
   def last
+    self.empty? ? nil : @tail.prev
   end
 
   def empty?
+    @head.next == @tail && @tail.prev == @head
+
   end
 
   def get(key)
+    self.each do |node|
+      return node.val if node.key == key
+    end
   end
 
   def include?(key)
+    self.each do |node|
+      return true if (node.key == key)
+    end
+    false
   end
 
   def append(key, val)
+    new_node = Node.new(key, val)
+    oldlast = @tail.prev
+    oldlast.next = new_node
+    new_node.prev = oldlast
+    new_node.next = @tail
+    @tail.prev = new_node
   end
 
   def update(key, val)
+    self.each do |node|
+      if (node.key == key)
+        node.val = val
+        return
+      end
+    end
   end
 
   def remove(key)
+    self.each do |node|
+      if (node.key == key)
+        node.remove
+        return
+      end
+    end
   end
 
-  def each
+  def each(&prc)
+
+    return if self.empty?
+
+    currpos = self.first
+
+    until(currpos == @tail)
+      prc.call(currpos)
+      currpos = currpos.next
+    end
+
   end
 
   # uncomment when you have `each` working and `Enumerable` included
